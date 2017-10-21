0ef20f00c2e781951b45618e7cbde672db743714
William Dewing <dewing.will@gmail.com>
2017-10-21 12:15:52 -0500

    Added Caves and trees
    -Added caves, trees, and dirt to
     generation
    -Optimized chunk loading and
     generation

--- Assets/Scripts/Voxels/TerrainGen.cs
+++ Assets/Scripts/Voxels/TerrainGen.cs
@@ -17,9 +17,15 @@ public class TerrainGen
     float dirtNoise = 0.04f;
     float dirtNoiseHeight = 3;
 
+    float caveFrequency = 0.025f;
+    int caveSize = 7;
+
+    float treeFrequency = 0.2f;
+    int treeDensity = 3;
+
     public Chunk ChunkGen(Chunk chunk)
     {
-        for (int x = chunk.pos.x; x < chunk.pos.x + Chunk.chunkSize; x++)
+        for (int x = chunk.pos.x - 3; x < chunk.pos.x + Chunk.chunkSize + 3; x++)
         {
             for (int z = chunk.pos.z; z < chunk.pos.z + Chunk.chunkSize; z++)
             {
@@ -42,19 +48,31 @@ public class TerrainGen
         int dirtHeight = stoneHeight + Mathf.FloorToInt(dirtBaseHeight);
         dirtHeight += GetNoise(x, 100, z, dirtNoise, Mathf.FloorToInt(dirtNoiseHeight));
 
-        for (int y = chunk.pos.y; y < chunk.pos.y + Chunk.chunkSize; y++)
+        for (int y = chunk.pos.y - 8; y < chunk.pos.y + Chunk.chunkSize; y++)
         {
-            if (y <= stoneHeight)
+            int caveChance = GetNoise(x, y, z, caveFrequency, 100);
+            if (y <= stoneHeight && caveSize < caveChance)
             {
-                chunk.SetBlock(x - chunk.pos.x, y - chunk.pos.y, z - chunk.pos.z, new Block());
+                SetBlock(x, y, z, new Block(), chunk);
             }
-            else if (y <= dirtHeight)
+            else if (y <= dirtHeight && caveSize < caveChance)
             {
-                chunk.SetBlock(x - chunk.pos.x, y - chunk.pos.y, z - chunk.pos.z, new BlockGrass());
+                if (y == dirtHeight)
+                {
+                    SetBlock(x, y, z, new BlockGrass(), chunk);
+                    if (GetNoise(x, 0, z, treeFrequency, 100) < treeDensity)
+                    {
+                        CreateTree(x, y + 1, z, chunk);
+                    }
+                }
+                else
+                {
+                    SetBlock(x, y, z, new BlockDirt(), chunk);
+                }
             }
             else
             {
-                chunk.SetBlock(x - chunk.pos.x, y - chunk.pos.y, z - chunk.pos.z, new BlockAir());
+                SetBlock(x, y, z, new BlockAir(), chunk);
             }
 
         }
@@ -65,5 +83,40 @@ public class TerrainGen
     public static int GetNoise(int x, int y, int z, float scale, int max)
     {
         return Mathf.FloorToInt((Noise.Generate(x * scale, y * scale, z * scale) + 1f) * (max / 2f));
+    }
+
+    public static void SetBlock(int x, int y, int z, Block block, Chunk chunk, bool replaceBlocks = false)
+    {
+        x -= chunk.pos.x;
+        y -= chunk.pos.y;
+        z -= chunk.pos.z;
+        if (Chunk.InRange(x) && Chunk.InRange(y) && Chunk.InRange(z))
+        {
+            if (replaceBlocks || chunk.blocks[x, y, z] == null)
+                chunk.SetBlock(x, y, z, block);
+        }
+    }
+
+    void CreateTree(int x, int y, int z, Chunk chunk)
+    {
+        //create leaves
+        for (int xi = -2; xi <= 2; xi++)
+        {
+            for (int yi = 4; yi <= 8; yi++)
+            {
+                for (int zi = -2; zi <= 2; zi++)
+                {
+                    if (Vector3.Distance(new Vector3(x, y + 6, z), new Vector3(x + xi, y + yi, z + zi)) <= 3)
+                    {
+                        SetBlock(x + xi, y + yi, z + zi, new BlockLeaves(), chunk, true);
+                    }
+                }
+            }
+        }
+        //create trunk
+        for (int yt = 0; yt < 6; yt++)
+        {
+            SetBlock(x, y + yt, z, new BlockWood(), chunk, true);
+        }
     }
 }
